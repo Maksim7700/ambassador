f2fb01e3c14fe9187093513df8beac0a73bf846c
Maksym Kuziv <mkuziv@hubspot.com>
2024-06-02 13:11:35 +0300

    Refactoring

--- src/components/Accept.jsx
+++ src/components/Accept.jsx
@@ -2,6 +2,7 @@ const Accept = () => {
   return (
     <><div className='accept-title'><div className='unbounded-700'>ЯК ПОДАТИСЬ?</div></div>
     <div className='accept-info'>
+      <div className='accept-info-inside-box'>
       <div className='accept-desc l-h-27'>
         <div><div className='mulish-400'>1. Завантажте форму заявки та бюджету.</div></div>
         <div><div className='mulish-400'>2. Надішліть заповнені документи на <u><b>grants@dyma.foundation</b></u> з темою листа «АмбасадориДрогобича_Заявка на ґрант”</div></div>
@@ -9,6 +10,7 @@ const Accept = () => {
       <div className='accept-buttons'>
         <button><div className='unbounded-700'>Офіційні Правила</div></button>
         <button><div className='unbounded-700'>Отримати Підписку</div></button>
+      </div>
       </div>
     </div></>
   )
